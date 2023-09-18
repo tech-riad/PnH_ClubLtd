57eeb369cca930106ca7a902b970caaadefdf06b
Md. Riad Ahmed <110854493+tech-riad@users.noreply.github.com>
2023-09-18 18:03:27 +0600

    login-auth-complete

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('backend.index');
+    return view('frontend.index');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
