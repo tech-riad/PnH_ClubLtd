70f3585395dbc00d9985082c5e787473d646ace3
Md. Riad Ahmed <110854493+tech-riad@users.noreply.github.com>
2023-09-27 15:30:58 +0600

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,15 +11,9 @@
     <!-- plugins:css -->
     <link rel="stylesheet" href="{{asset('backend/assets/vendors/mdi/css/materialdesignicons.min.css')}}">
     <link rel="stylesheet" href="{{asset('backend/assets/vendors/css/vendor.bundle.base.css')}}">
-    <!-- endinject -->
-    <!-- Plugin css for this page -->
-    <!-- End plugin css for this page -->
-    <!-- inject:css -->
-    <!-- endinject -->
-    <!-- Layout styles -->
+
     <link rel="stylesheet" href="{{asset('backend/assets/css/style.css')}}">
-    <!-- End layout styles -->
-    {{-- <link rel="shortcut icon" href="{{asset('backend/assets/images/favicon.ico')}}" /> --}}
+    
   </head>
   <body>
     <div class="container-scroller">
