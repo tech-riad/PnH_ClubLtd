ae823c92e5c2d9579cc275f395749b74a54dffe9
Md. Riad Ahmed <110854493+tech-riad@users.noreply.github.com>
2023-10-07 16:05:42 +0600

    login-update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,82 @@
-
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" type="image/png" href="{{asset(helper::getLatestsetting()->favicon ?? Null)}}">
-    <title>{{helper::metaInformation()->title}}</title>
+    <link rel="icon" type="image/png" href="{{ asset(helper::getLatestsetting()->favicon ?? Null) }}">
+    <title>{{ helper::metaInformation()->title }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- plugins:css -->
-    <link rel="stylesheet" href="{{asset('backend/assets/vendors/mdi/css/materialdesignicons.min.css')}}">
-    <link rel="stylesheet" href="{{asset('backend/assets/vendors/css/vendor.bundle.base.css')}}">
+    <link rel="stylesheet"
+        href="{{ asset('backend/assets/vendors/mdi/css/materialdesignicons.min.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('backend/assets/vendors/css/vendor.bundle.base.css') }}">
 
-    <link rel="stylesheet" href="{{asset('backend/assets/css/style.css')}}">
-    
-  </head>
-  <body>
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/style.css') }}">
+
+</head>
+
+<body>
     <div class="container-scroller">
-      <div class="container-fluid page-body-wrapper full-page-wrapper">
-        <div class="content-wrapper d-flex align-items-center auth">
-          <div class="row flex-grow">
-            <div class="col-lg-4 mx-auto">
-              <div class="auth-form-light text-left p-5">
-                <div class="brand-logo">
-                  <img src="{{asset('backend/assets/images/logo.svg')}}">
-                </div>
-                <h4>Hello! let's get started</h4>
-                <h6 class="font-weight-light">Sign in to continue.</h6>
-                <form class="pt-3" method="POST" action="{{ route('login') }}">
-                    @csrf
-                  <div class="form-group">
-                    <input type="email" class="form-control form-control-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                  </div>
-                  <div class="form-group">
-                    <input type="password" class="form-control form-control-lg @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                  </div>
-                  <div class="mt-3">
-                    <button class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn" type="submit">SIGN IN</button>
-                  </div>
-                  <div class="my-2 d-flex justify-content-between align-items-center">
-                    <div class="form-check">
-                      <label class="form-check-label text-muted">
-                        <input type="checkbox" class="form-check-input"> Keep me signed in </label>
+        <div class="container-fluid page-body-wrapper full-page-wrapper">
+            <div class="content-wrapper d-flex align-items-center auth">
+                <div class="row flex-grow">
+                    <div class="col-lg-4 mx-auto">
+                        <div class="auth-form-light text-left p-5">
+                            <div class="brand-logo">
+                                <img src="{{ asset('backend/assets/images/logo.svg') }}">
+                            </div>
+                            <h4>Hello! let's get started</h4>
+                            <h6 class="font-weight-light">Sign in to continue.</h6>
+                            <form class="pt-3" method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <input type="email"
+                                        class="form-control form-control-lg @error('email') is-invalid @enderror"
+                                        name="email" value="{{ old('email') }}" required
+                                        autocomplete="email" autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <input type="password"
+                                        class="form-control form-control-lg @error('password') is-invalid @enderror"
+                                        name="password" required autocomplete="password">
+                                </div>
+                                <div class="mt-3">
+                                    <button
+                                        class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn"
+                                        type="submit">SIGN IN</button>
+                                </div>
+                                <div class="my-2 d-flex justify-content-between align-items-center">
+                                    <div class="form-check">
+                                        <label class="form-check-label text-muted">
+                                            <input type="checkbox" class="form-check-input"> Keep me signed in </label>
+                                    </div>
+                                    {{-- <a href="#" class="auth-link text-black">Forgot password?</a> --}}
+                                </div>
+                                {{-- <div class="text-center mt-4 font-weight-light"> Don't have an account? <a href="register.html" class="text-primary">Create</a> --}}
+                        
+                        </form>
                     </div>
-                    {{-- <a href="#" class="auth-link text-black">Forgot password?</a> --}}
-                  </div>
-                  {{-- <div class="text-center mt-4 font-weight-light"> Don't have an account? <a href="register.html" class="text-primary">Create</a> --}}
-                  </div>
-                </form>
-              </div>
+                </div>
             </div>
-          </div>
         </div>
         <!-- content-wrapper ends -->
-      </div>
-      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
     </div>
     <!-- container-scroller -->
     <!-- plugins:js -->
-    <script src="{{asset('backend/assets/vendors/js/vendor.bundle.base.js')}}"></script>
+    <script src="{{ asset('backend/assets/vendors/js/vendor.bundle.base.js') }}"></script>
     <!-- endinject -->
     <!-- Plugin js for this page -->
     <!-- End plugin js for this page -->
     <!-- inject:js -->
-    <script src="{{asset('backend/assets/js/off-canvas.js')}}"></script>
-    <script src="{{asset('backend/assets/js/hoverable-collapse.js')}}"></script>
-    <script src="{{asset('backend/assets/js/misc.js')}}"></script>
+    <script src="{{ asset('backend/assets/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/misc.js') }}"></script>
     <!-- endinject -->
-  </body>
+</body>
+
 </html>
